631fb58bacb9bd772ab78c95aa030813de171df5
Silvio Moioli <silvio@moioli.net>
2024-11-20 13:41:25 +0100

    deploy.sh: improve output, change mode
    Signed-off-by: Silvio Moioli <silvio@moioli.net>

--- deploy.sh
+++ deploy.sh
@@ -8,13 +8,17 @@ tofu apply --auto-approve
 export KUBECONFIG=`pwd`/config/cluster.yaml
 
 helm repo add nvidia https://helm.ngc.nvidia.com/nvidia \
-    && helm repo update
+   && helm repo update
 
 helm install gpu-operator -n gpu-operator \
-  --create-namespace nvidia/gpu-operator \
-  -f helm/values.yaml
+ --create-namespace nvidia/gpu-operator \
+ -f helm/values.yaml
 
+echo
 echo ACCESS DETAILS:
 echo
-echo \*\*\* AI CLUSTER
+echo \*\*\* NODES:
+ls -1 config/ssh-to-*
+echo
+echo \*\*\* CLUSTER:
 echo export KUBECONFIG=`pwd`/config/cluster.yaml
